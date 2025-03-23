354d067efed90b7022b3a64673557f19169b2abc
Kamila Paulina Godlewska <kamapago@gmail.com>
2025-03-23 15:43:11 +0100

    Update pasekPostepu.js

--- assets/pasekPostepu.js
+++ assets/pasekPostepu.js
@@ -1,20 +1,15 @@
-console.log("Plik JavaScript został załadowany!");
-
 document.addEventListener('DOMContentLoaded', function() {
     const checkboxes = document.querySelectorAll('.zadania');
     const pasekPostepu = document.getElementById('postep');
     const procentPostepu = document.getElementById('procent');
-    console.log("DOMContentLoaded!");
 
-    // Dodaj logi, aby sprawdzić, czy elementy istnieją
-    console.log(pasekPostepu); // Powinno pokazać element <progress>
-    console.log(procentPostepu); // Powinno pokazać element <span>
-    console.log(checkboxes); // Powinno pokazać NodeList z wszystkimi checkboxami
 
     function aktualizujPostep() {
         const wykonaneZadania = Array.from(checkboxes).filter(checkbox => checkbox.checked).length;
         const postep = (wykonaneZadania / checkboxes.length) * 100;
         console.log(`Wykonane zadania: ${wykonaneZadania}, Postęp: ${postep}%`);
+
+        console.log(checkboxes);
         
         if (pasekPostepu) {
     
